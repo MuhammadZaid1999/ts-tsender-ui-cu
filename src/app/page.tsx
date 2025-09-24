f7da606129b176b9bb1edea510450869bf3f90e5
MuhammadZaid1999 <zaid.aptechiic@gmail.com>
2025-09-25 01:53:07 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
+'use client'; // Required for client-side hooks like useAccount
 import AirdropForm from "@/components/AirdropForm";
+import HomeContent from "@/components/ui/HomeContent";
+import { useAccount } from "wagmi"; // Import the wagmi hook 
 
 export default function Home() {
+    const {isConnected} = useAccount(); // Example usage of wagmi hook
+    
     return (
         <main className="p-4">
             {/* The Header is already handled by layout.tsx */}
-            <AirdropForm />
+            {isConnected ? (
+                <div>
+                    <HomeContent />
+                </div> 
+            ) : (
+                <div className="justify-center text-center text-zinc-800">
+                    Please connect a wallet...
+                </div>
+            )}
         </main>
     )
 }
